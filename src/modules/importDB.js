d7c79f11ea798331b04beab66421323c2e9288ed
ah1 <achille.huenenberger@inteco.ch>
2025-05-06 08:48:26 +0200

    improved import_dump: automatic archive recognition and importing without recreating db

--- src/modules/importDB.js
+++ src/modules/importDB.js
@@ -1,5 +1,5 @@
 import prompts from "prompts"
-import { writeFileSync, readFileSync, readdirSync, writeFile, unlinkSync, mkdirSync, renameSync, rmSync } from "fs";
+import { writeFileSync, readFileSync, readdirSync, writeFile, unlinkSync, mkdirSync, renameSync, rmSync, copyFileSync } from "fs";
 import Seven from 'node-7z'
 import ora from "ora";
 import { DB } from "../utils/db/DB.js";
@@ -10,12 +10,12 @@ import path from "path";
 import sevenBin from '7zip-bin'
 
 
-export default async function importDB(cli){
+export default async function importDB(cli) {
     console.log()
 
     const config = await Config.getConfig();
 
-    const archivesDirectories = readdirSync(process.cwd(), { withFileTypes: true }).filter(dirent => dirent.isFile()).map(dirent => { return { title: dirent.name } })
+    const files = readdirSync(process.cwd(), { withFileTypes: true }).filter(dirent => dirent.isFile()).map(dirent => { return { title: dirent.name } })
 
     let success = true;
 
@@ -23,17 +23,25 @@ export default async function importDB(cli){
         {
             // Ordnerauswahl von vorhandenen Ordner in configIndividual
             type: 'autocomplete',
-            name: 'archive',
-            message: 'Archive-Name?',
-            choices: archivesDirectories
+            name: 'file',
+            message: 'Dump-Name?',
+            choices: files
         },
         {
             // Ordnerauswahl von vorhandenen Ordner in configIndividual
             type: 'text',
             name: 'dbName',
             message: 'DB-Name?'
+        },
+        {
+            type: 'toggle',
+            name: 'dropDB',
+            message: 'Delete and Recreate DB before import?',
+            initial: true,
+            active: 'yes',
+            inactive: 'no'
         }
-    ],{
+    ], {
         onCancel: () => {
             console.log()
             console.log(chalk.red("Cancelled Import!"))
@@ -42,42 +50,72 @@ export default async function importDB(cli){
         }
     })
 
-    if(success){
+    if (success) {
         console.log()
-        const spinnerZIP = ora('Unpacking Archive').start();
-        
-        rmSync("."+path.sep+"dump", {recursive:true, force:true})
-        mkdirSync("."+path.sep+"/dump", {recursive:true})
+        rmSync("." + path.sep + "dump", { recursive: true, force: true })
+        mkdirSync("." + path.sep + "/dump", { recursive: true })
 
-        const unpack = Seven.extract(results.archive, "."+path.sep+"dump", {
-            $bin: sevenBin.path7za
-        })
+        if (await isArchive(results.file)) {
 
-        unpack.on('end', async()=>{
-            const filename = readdirSync("."+path.sep+"dump")[0]
-            renameSync("."+path.sep+"dump"+path.sep+""+filename, "."+path.sep+"dump"+path.sep+"dump.sql")
-            spinnerZIP.succeed("Archive unpacked")
+            const spinnerZIP = ora('Unpacking Archive').start();
 
-            const spinnerImport = ora('Importing DB').start();
-    
-            await DB.executeQuery("DROP DATABASE IF EXISTS "+results.dbName);
-            await DB.executeQuery("CREATE DATABASE "+results.dbName);
-    
-            exec("mysql -u "+config.dbUser+" -p"+config.dbPassword+" "+results.dbName+" < dump"+path.sep+"dump.sql", (error, stdout, stderr) => {
-                if(error != null){
-                    spinnerImport.fail("Database-Import failed!")
-                    console.log(error)
-                }else{
-                    spinnerImport.succeed("Database imported")
-
-                    const deleteSpinner = ora("Delete temporary dump").start()
-                    rmSync("."+path.sep+"dump", {recursive:true, force:true})
-                    deleteSpinner.succeed("Dump deleted")
-                }
-
-                console.log()
+            const unpack = Seven.extract(results.file, "." + path.sep + "dump", {
+                $bin: sevenBin.path7za
             })
-        })
 
+            unpack.on('end', async () => {
+                const filename = readdirSync("." + path.sep + "dump")[0]
+                renameSync("." + path.sep + "dump" + path.sep + "" + filename, "." + path.sep + "dump" + path.sep + "dump.sql")
+                spinnerZIP.succeed("Archive unpacked")
+
+                await importDBActual(config, results)
+            })
+        }else{
+            const spinnerCopy = ora('Copying Dump').start();
+
+            copyFileSync("."+path.sep+results.file, "." + path.sep + "dump" + path.sep + "dump.sql")
+
+            spinnerCopy.succeed("Dump copied")
+
+            await importDBActual(config, results)
+        }
     }
+}
+
+function isArchive(file) {
+    return new Promise((resolve, reject) => {
+        const test = Seven.list("."+path.sep+file, {
+            $bin: sevenBin.path7za
+        });
+        test.on('end', (stream) => {
+            resolve(true)
+        })
+        test.on('error', (stream) => {
+            resolve(false)
+        })
+    });
+}
+
+async function importDBActual(config, results) {
+    const spinnerImport = ora('Importing DB').start();
+
+    if (results.dropDB) {
+        await DB.executeQuery("DROP DATABASE IF EXISTS " + results.dbName);
+        await DB.executeQuery("CREATE DATABASE " + results.dbName);
+    }
+
+    exec("mysql -u " + config.dbUser + " -p" + config.dbPassword + " " + results.dbName + " < dump" + path.sep + "dump.sql", (error, stdout, stderr) => {
+        if (error != null) {
+            spinnerImport.fail("Database-Import failed!")
+            console.log(error)
+        } else {
+            spinnerImport.succeed("Database imported")
+
+            const deleteSpinner = ora("Delete temporary dump").start()
+            rmSync("." + path.sep + "dump", { recursive: true, force: true })
+            deleteSpinner.succeed("Dump deleted")
+        }
+
+        console.log()
+    })
 }
