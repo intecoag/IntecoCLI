be013e620e21d924f2b52a6c8e08cc3afad1f220
lb2 <lorin.baumeister@inteco.ch>
2026-08-12 14:09:13 +0200

    feat: add autocompletion to mnr selection and add support for updating user across all mands

--- src/modules/t003Rewrite.ts
+++ src/modules/t003Rewrite.ts
@@ -11,6 +11,11 @@ type UserPrompt = {
     username: string;
 };
 
+type MandRow = {
+    mand_mandant: string;
+    mand_name: string;
+}
+
 export default async function t003Rewrite(_cli: unknown): Promise<void> {
     console.log()
 
@@ -20,54 +25,69 @@ export default async function t003Rewrite(_cli: unknown): Promise<void> {
 
     let success = true;
 
-    const responseDB = await prompts([{
+    const promptOptions = {
+        onCancel: () => {
+            console.log();
+            console.log(chalk.red("Cancelled Rewrite!"));
+            console.log();
+            success = false;
+        }
+    };
+
+    const responseDB = await prompts({
         // DB-Auswahl von DB
         type: 'autocomplete',
         name: 'dbName',
         message: 'DB-Name?',
-        choices: databaseNames.map(name => { return { title: name.name } })
-    }, {
-        type: 'number',
+        choices: databaseNames.map(db => ({
+            title: db.name,
+            value: db.name
+        }))
+    }, promptOptions);
+
+    if(!success)
+        return;
+
+    const mands = await DB.executeQueryOnDB(`SELECT mand_mandant, mand_name FROM mand ORDER BY mand_mandant`, responseDB.dbName) as MandRow[];
+
+    const responseMand = await prompts({
+        type: 'autocomplete',
         name: 'mnr',
         message: 'Mandant?',
-        initial: '1'
-    }], {
-        onCancel: () => {
-            console.log()
-            console.log(chalk.red("Cancelled Rewrite!"))
-            console.log()
-            success = false
-        }
-    });
+        choices: mands.map(mand => ({
+            title: `${mand.mand_mandant}: ${mand.mand_name}`,
+            value: mand.mand_mandant
+        })).concat([{ title: 'All', value: "ALL" }])
+    }, promptOptions);
+
+    if(!success)
+        return;
+
+    const isAll = responseMand.mnr === "ALL";
+
+    const users = isAll
+        ? await DB.executeQueryOnDB("SELECT DISTINCT t003_pw FROM t003", responseDB.dbName) as UserRow[]
+        : await DB.executeQueryOnDB("SELECT t003_pw FROM t003 WHERE t003_mnr = " + responseMand.mnr, responseDB.dbName) as UserRow[];
+
+    const responses = await prompts([{
+        // Ordnerauswahl von vorhandenen Ordner in configIndividual
+        type: 'autocomplete',
+        name: 'username',
+        message: 'Username?',
+        choices: users.map(entry => { return ({ title: entry.t003_pw }) })
+    }], promptOptions) as UserPrompt
 
     if (success) {
+        console.log()
 
-        const users = await DB.executeQueryOnDB("SELECT t003_pw FROM t003 WHERE t003_mnr = " + responseDB.mnr, responseDB.dbName) as UserRow[];
-
-
-        const responses = await prompts([{
-            // Ordnerauswahl von vorhandenen Ordner in configIndividual
-            type: 'autocomplete',
-            name: 'username',
-            message: 'Username?',
-            choices: users.map(entry => { return ({ title: entry.t003_pw }) })
-        }], {
-            onCancel: () => {
-                console.log()
-                console.log(chalk.red("Cancelled Rewrite!"))
-                console.log()
-                success = false
-            }
-        }) as UserPrompt
-
-        if (success) {
-            console.log()
-
-            await DB.executeQueryOnDB("UPDATE t003 SET t003_pw = '"+config.wegasUsername+"' WHERE t003_mnr = "+responseDB.mnr+" AND t003_pw = '"+responses.username+"'", responseDB.dbName);
-
-            console.log(chalk.green("T003 rewritten!"))
-            console.log()
+        if(isAll) {
+            await DB.executeQueryOnDB("UPDATE t003 SET t003_pw = '"+config.wegasUsername+"' WHERE t003_pw = '"+responses.username+"'", responseDB.dbName);
         }
+        else {
+            await DB.executeQueryOnDB("UPDATE t003 SET t003_pw = '"+config.wegasUsername+"' WHERE t003_mnr = "+responseMand.mnr+" AND t003_pw = '"+responses.username+"'", responseDB.dbName);
+        }
+
+        console.log(chalk.green("T003 rewritten!"))
+        console.log()
     }
 }
-
